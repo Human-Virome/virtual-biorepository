272ed662d0db9d9029c4efe496bcae1e3d6c4728
Daniel Smith <dansmith@orst.edu>
2026-04-16 22:22:59 +0100

    missing comma

--- html/upload.js
+++ html/upload.js
@@ -15,7 +15,7 @@ $( document ).ready(function() {
       $('#md_file_status').html('<span aria-busy="true" aria-invalid="false">validating...</span>');
       
       api({
-        action   : 'metadata_upload'
+        action   : 'metadata_upload',
         file     : 'md_file_input',
         payload  : { save: false },
         callback : function (resp) {
