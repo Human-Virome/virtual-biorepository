b4ecceb4555b95321c3f48bfd67ea71d91e6c3ac
Daniel Smith <dansmith@orst.edu>
2026-04-17 19:56:27 +0100

    `as.list(req$HEADERS)`

--- api/handler.r
+++ api/handler.r
@@ -18,7 +18,7 @@ invisible(sapply(list.files("R", pattern = "\\.r$", full.names = TRUE), source))
 # Initialize the empty Plumber router.
 pr <- plumber::pr()
 
-pr <- plumber::pr_get(pr, '/req', handler = function (req) { req })
+pr <- plumber::pr_get(pr, '/req', handler = function (req) { as.list(req$HEADERS) })
 
 
 # Dynamically mount routes to functions starting with "api_".
