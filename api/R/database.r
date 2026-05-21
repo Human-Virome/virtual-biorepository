9cbfc9fd738589c78dbce534be7f74824046f706
Daniel Smith <dansmith@orst.edu>
2026-05-21 01:05:35 +0100

    special `SHOW` handling

--- api/R/database.r
+++ api/R/database.r
@@ -35,7 +35,7 @@ db_query <- function (db, sql, err_code, params = NULL, simplify = TRUE, req1 = 
       #________________________________________________________
       params <- if (length(params)) unname(params) else NULL
       result <- do.call(
-        what = if (verb == "SELECT") DBI::dbGetQuery else DBI::dbExecute, 
+        what = if (verb %in% c("SELECT", "SHOW")) DBI::dbGetQuery else DBI::dbExecute, 
         args = list(conn = db, statement = sql, params = params) )
       
       
